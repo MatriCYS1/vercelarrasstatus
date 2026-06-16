b25194743f9d29aedc3e4b7da7aa8ef17f44e9a3
MatriCYS1 <MatriCYS.1@gmail.com>
2026-06-16 22:55:38 +0300

    Update index.js

--- nexus-clone/nexus-ldm/index.js
+++ nexus-clone/nexus-ldm/index.js
@@ -301,7 +301,7 @@ function renderPlayer(p) {
   let colorId;
   if (safeName === "Testing") {
     colorId = 5;
-  } else if (safeName === "h#shtag") {
+  } else if (safeName === "desmos") {
     colorId = 6;
   } else {
     colorId = safeName.split("").reduce((acc, cur) => acc + cur.codePointAt(0), 0) % 5;
